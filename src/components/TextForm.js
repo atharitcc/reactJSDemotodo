8c9f0b6c49b768b4c91ee422204f59a78368ee28
atharmarfat <athar@devsinindia.com>
2024-02-06 11:30:07 +0530

    state & preview words

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,14 +13,22 @@ export default function TextForm(props) {
         setText(event.target.value);
     }
     const [text, setText] = useState('Enter text here');
+    var wordCount = text.split(" ").length;
     return (
         <>
-        <div>
+        <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
             <button className='btn btn-primary' onClick={handleUpCLick}>Convert to Uppercase</button>
+        </div>
+        <div className='container my-3'>
+            <h1>Your text summary</h1>
+            <p>{wordCount} words and {text.length} charcters</p>
+            <p>{0.008 * wordCount } Minutes read</p>
+            <h3>Preview</h3>
+            <p>{text}</p>
         </div>
         </>
     )
