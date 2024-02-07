349e484a08de8bcd414c7f7626376720b2c1e337
atharmarfat <athar@devsinindia.com>
2024-02-07 16:25:36 +0530

    state & props

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
@@ -5,10 +6,11 @@ import TextForm from './components/TextForm';
 
 
 function App() {
+  const [mode, setMode] = useState("dark"); // whether dark mode is enable or not
   return (
     <>
       {/* <Navbar title="Textutils" aboutText="About Textutils"/> */}
-      <Navbar title="Textutils"/>
+      <Navbar title="Textutils" mode={mode}/>
       <div className='container my-3'>
         <TextForm heading="Enter the text to analyze belows"/>
         {/* <About/> */}
